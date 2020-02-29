27ba851a3dc98c9568d342755293872f45785376
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-02-29 16:42:54 +0200

    *[MapGenerator] updated generation of ground for do not create overrider objects.

--- TD_Ellemental/Assets/Code/Level/MapGenerator.cs
+++ TD_Ellemental/Assets/Code/Level/MapGenerator.cs
@@ -9,7 +9,10 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] MapGeneratorItem[] m_items;
     [SerializeField] Texture2D m_testMap;
     [SerializeField] NavMeshSurface m_surface;
-    [SerializeField] GameObject m_fillPrefab;
+    [SerializeField] GameObject m_groundPrefab;
+    [SerializeField] bool m_isGroundScale = true;
+
+    List<GameObject> m_grounds = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -38,20 +41,101 @@ public class MapGenerator : MonoBehaviour
     }
 
 
+    private void RemoveGrounds()
+    {
+        foreach (var ground in m_grounds)
+        {
+            Destroy(ground);
+        }
+        m_grounds.Clear();
+    }
+
     private void LoadMap(Texture2D a_map)
     {
+        RemoveGrounds();
+        float offsetY = 0;
+        GameObject groundTile;
+        if (m_groundPrefab)
+        {
+            groundTile = Instantiate(m_groundPrefab, Vector3.zero, Quaternion.identity, transform);
+            var renderer = groundTile.GetComponent<Renderer>();
+            if (renderer)
+            {
+                offsetY = renderer.bounds.max.y;
+                if (m_isGroundScale)
+                {
+                    float scaleX = a_map.width / renderer.bounds.size.x;
+                    float scaleZ = a_map.height / renderer.bounds.size.z;
+                    groundTile.transform.localScale = new Vector3(scaleX, 1, scaleZ);
+                    groundTile.transform.position = new Vector3(renderer.bounds.size.x/2, 0, renderer.bounds.size.z / 2);
+                    m_grounds.Add(groundTile);
+                    Debug.Log($"ScaleX = {scaleX}; ScaleZ = {scaleZ}");
+                }
+                else
+                {
+                    int amountX = Mathf.RoundToInt(a_map.width / renderer.bounds.size.x );
+                    int amountZ = Mathf.RoundToInt( a_map.height / renderer.bounds.size.z );
+
+                    float scaleX = a_map.width / amountX / renderer.bounds.size.x;
+                    float scaleZ = a_map.height / amountZ / renderer.bounds.size.z;
+
+                    Debug.Log($"amountX = {amountX};\t amountZ = {amountZ};\t ScaleX = {scaleX};\t ScaleZ = {scaleZ}");
+
+                    float[] scales = { scaleX, scaleZ };
+                    float sumAmountRound = float.MaxValue;
+                    float resultScale = 1;
+                    foreach ( float scale in scales )
+                    {
+                        float amountfX = a_map.width / renderer.bounds.size.x / scale;
+                        float amountfZ = a_map.height / renderer.bounds.size.z / scale;
+
+                        float roundSum = Mathf.Abs(amountfX - Mathf.Round(amountfX)) + Mathf.Abs(amountfZ - Mathf.Round(amountfZ));
+                        if (roundSum < sumAmountRound)
+                        {
+                            sumAmountRound = roundSum;
+                            resultScale = scale;
+                        }
+                    }
+
+                    amountX = Mathf.RoundToInt(a_map.width / renderer.bounds.size.x / resultScale);
+                    amountZ = Mathf.RoundToInt(a_map.height / renderer.bounds.size.z / resultScale);
+
+                    scaleX = a_map.width / amountX / renderer.bounds.size.x;
+                    scaleZ = a_map.height / amountZ / renderer.bounds.size.z;
+
+                    Debug.Log($"PART2: amountX = {amountX};\t amountZ = {amountZ};\t ScaleX = {scaleX};\t ScaleZ = {scaleZ}");
+
+                    groundTile.transform.localScale = new Vector3(scaleX, 1, scaleZ);
+
+                    float stepX = scaleX * renderer.bounds.size.x;
+                    float stepZ = scaleZ * renderer.bounds.size.z;
+
+                    Destroy(groundTile);
+                    //groundTile.transform.position = new Vector3(stepX/2, 0, stepZ / 2);
+                    //m_grounds.Add(groundTile);
+
+                    for ( float x = stepX / 2; x < a_map.width; x+= stepX)
+                    {
+                        for (float z = stepZ/2; z < a_map.height; z += stepZ)
+                        {
+                            Vector3 position = new Vector3(x, 0, z);
+                            groundTile = Instantiate(m_groundPrefab, position, Quaternion.identity, transform);
+                            groundTile.transform.localScale = new Vector3(scaleX, 1, scaleZ);
+                            m_grounds.Add(groundTile);
+                        }
+                    }
+                }
+            }
+
+        }
+
+
         for (int i = 0; i < a_map.width; ++i)
         {
             for (int j = 0; j < a_map.height; ++j)
             {
                 Color testColor = a_map.GetPixel(i, j);
-                Vector3 position = new Vector3(i, 0, j);
-                var fillTile = Instantiate(m_fillPrefab, position, Quaternion.identity, transform);
-                var renderer = fillTile.GetComponent<Renderer>();
-                if ( renderer )
-                {
-                    position.y = renderer.bounds.max.y;
-                }
+                Vector3 position = new Vector3(i, offsetY, j);
                 LoadMapTile(i, j, a_map.GetPixel(i, j), position);
             }
         }
---
+++ TD_Ellemental/Assets/MiniMap/MiniMapCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamera : MonoBehaviour
+{
+    public Transform m_target;
+    //private float m_initialY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // m_player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (m_target)
+        {
+            Vector3 newPosition = m_target.transform.position;
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+        }
+
+        //roatteion of camera same as player rotation but it is not needed for me.
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, m_player.eulerAngles.y, transform.rotation.eulerAngles.z);
+    }
+}
