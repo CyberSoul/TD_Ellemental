60ae83a02e6712e1586520ff9389c52603955e5e
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-03-01 22:30:29 +0200

    -added slider for time multiplier

--- TD_Ellemental/Assets/Code/UI/IngameMenuController.cs
+++ TD_Ellemental/Assets/Code/UI/IngameMenuController.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IngameMenuController : MonoBehaviour
 {
+    [SerializeField] Slider m_timeSlider;
+    [SerializeField] Text m_timeValue;
+
+    private void Start()
+    {
+        m_timeSlider.onValueChanged.AddListener(ChangeTimeMultiplier);
+    }
+
     public void BackPressed()
     {
         SceneManager.LoadScene((int)SceneId.Menu, LoadSceneMode.Single);
+    }
+
+    public void ChangeTimeMultiplier(float a_newMultiplier)
+    {
+        Time.timeScale = a_newMultiplier;
+        m_timeValue.text = a_newMultiplier.ToString("0.00");
     }
 }
