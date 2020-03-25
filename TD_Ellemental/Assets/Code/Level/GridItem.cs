9380e2c075ff2f2c1c802545be7ea0f93e2862e3
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-03-25 13:00:58 +0200

    *backup

---
+++ TD_Ellemental/Assets/Code/Level/GridItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridItem : MonoBehaviour
+{
+    public enum AvailabilityState
+    {
+        Free,
+        Unavailable,
+        Occupied,
+
+        Count
+    }
+
+    [SerializeField] 
+
+    protected AvailabilityState m_availability = AvailabilityState.Free;
+    public AvailabilityState Availability
+    {
+        get { return m_availability; }
+        set { m_availability = value; }
+    }
+    [SerializeField] Gradient m_availabilityColors;
+
+    /*[SerializeField] List<Color> m_availabilityColor = new List<Color>(Availability.Count);
+
+    [SerializeField] Color m_freeColor;
+    [SerializeField] Color m_UnavailableColor;
+    [SerializeField] Color m_freeColor;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ TD_Ellemental/Assets/Code/Player/PlayerMovementInputSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.InputSystem;
+
+public class PlayerMovementInputSystem : MonoBehaviour
+{
+    //[SerializeField] InputControlls m_inputControlls;
+    Vector3 m_moveDist = Vector3.zero;
+
+
+
+    private void Awake()
+    {
+        /*m_inputControlls = new InputControlls();
+        m_inputControlls.Camera.Move.performed += InputActionContext_Move;//ctx => m_moveDist = new Vector3( ctx.ReadValue<Vector2>();  */
+    }
+
+    private void OnEnable()
+    {
+        //m_inputControlls.Enable();
+    }
+
+    private void OnDisable()
+    {
+       // m_inputControlls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveTest()
+    {
+        Debug.Log("MoveTest");
+    }
+
+   /* public void InputActionContext_Move(InputAction.CallbackContext a_context)
+    {
+        Vector2 inputValue = a_context.ReadValue<Vector2>();
+        m_moveDist.x = inputValue.x;
+        m_moveDist.z = inputValue.y;
+        Debug.Log($"InputActionContext_Move: inputValue.x = {inputValue.x} inputValue.y = {inputValue.y}");
+    }*/
+}
